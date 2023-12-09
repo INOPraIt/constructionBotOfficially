9ab9a9ffc6d5e4d5d284cae712f6f672360c8c94
INOPraIt <inoprait@gmail.com>
2023-12-09 14:18:19 +0300

    dawd

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,21 @@ import '../../assets/style/HomeStyle.scss';
 export default function Home() {
   return (
     <div className='containerHome'>
-      xdawDAWDA
+      <h1>ВЫРЫК</h1>
+      {/* <p className='balansText'>
+        Баланс
+      </p>
+      <p className='balansRubl'>
+        0,00 Р
+      </p>
+      <div>
+        <button className='jobBtn'>
+          Работа
+        </button>
+        <button className='statusBtn'>
+          Статус
+        </button>
+      </div> */}
     </div>
   )
 };
